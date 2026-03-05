b33afd5f3d8787d439025ac316b9c01ed1105ae2
dev-j0e1 <corkj9213@gmail.com>
2026-03-05 12:47:56 +0800

    Made some visual improvements and dynamically render the links into the HTML with x-for, referencing the alpine data in main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 document.addEventListener("alpine:init", () => {
-    Alpine.data("header", () => {
+    Alpine.data("headerData", () => ({
+        mobileNavOpen: false,
+        links: [
+            {name: "Wordle Clone Project", href: "https://dev-j0e1.github.io/wordl-like-game-small-project/"},
+            {name: "Personal Website", href: "https://j0e1.dev"},
+            {name: "Github Profile", href: "https://github.com/dev-j0e1"},
+            {name: "Clash of Code Profile", href: "https://www.codingame.com/profile/c67bb3cbb1bd21889f0c836c5c7f8bd04321396"}
+        ]
         
-    })
+        })
+    )
+})
+
+
+document.addEventListener("keydown", e=> {
+    let dataElement = document.querySelector("[x-data]")
+    let navIsOpen = dataElement._x_dataStack[0].mobileNavOpen
+
+    if (navIsOpen && e.key === "Escape") {
+        dataElement._x_dataStack[0].mobileNavOpen = false
+    }
 })
